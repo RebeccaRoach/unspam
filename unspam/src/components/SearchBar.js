e0d971ef1058875f778ed759662ac3aa3a50c426
RebeccaRoach <rebeccaroach12@gmail.com>
2023-02-19 15:33:43 -0800

    Loads countries data from JSON instead of variable

--- unspam/src/components/SearchBar.js
+++ unspam/src/components/SearchBar.js
@@ -1,43 +1,16 @@
 import React, {useState} from 'react'
+import Countries from "../data/countries.json"
 
 
 const SearchBar = () => {
 
-    // Example for proof of concept
-    const countries = [
-        { name: "Belgium", continent: "Europe" },
-        { name: "India", continent: "Asia" },
-        { name: "Bolivia", continent: "South America" },
-        { name: "Ghana", continent: "Africa" },
-        { name: "Japan", continent: "Asia" },
-        { name: "Canada", continent: "North America" },
-        { name: "New Zealand", continent: "Australasia" },
-        { name: "Italy", continent: "Europe" },
-        { name: "South Africa", continent: "Africa" },
-        { name: "China", continent: "Asia" },
-        { name: "Paraguay", continent: "South America" },
-        { name: "Usa", continent: "North America" },
-        { name: "France", continent: "Europe" },
-        { name: "Botswana", continent: "Africa" },
-        { name: "Spain", continent: "Europe" },
-        { name: "Senegal", continent: "Africa" },
-        { name: "Brazil", continent: "South America" },
-        { name: "Denmark", continent: "Europe" },
-        { name: "Mexico", continent: "South America" },
-        { name: "Australia", continent: "Australasia" },
-        { name: "Tanzania", continent: "Africa" },
-        { name: "Bangladesh", continent: "Asia" },
-        { name: "Portugal", continent: "Europe" },
-        { name: "Pakistan", continent: "Asia" },
-    ];
-
-    const [filteredList, setFilteredList] = new useState(countries);
+    const [filteredList, setFilteredList] = new useState(Countries);
 
     const filterBySearch = (event) => {
         // Access input value
         const query = event.target.value;
         // Create copy of item list
-        var updatedList = [...countries];
+        var updatedList = [...Countries];
         // Include all elements which includes the search query
         updatedList = updatedList.filter((item) => 
             {return item.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;}
